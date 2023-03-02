1fc594e7613ea67814fc2ba3746d0456bd58451c
xutao <xutao@infinidata.cc>
2023-03-02 17:13:54 +0800

    init3

---
+++ src/views/components/test.vue
@@ -0,0 +1,21 @@
+<template>
+    <div vc>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onBeforeMount } from 'vue';
+
+
+import {computed} from 'vue'
+const name = computed(() => {
+    return 
+})
+
+
+</script>
+
+<style scoped>
+
+</style>
